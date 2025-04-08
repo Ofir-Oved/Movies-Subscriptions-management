8689a6aac0ad8f23306715166cbd20c34232f0fb
Ofir-Oved <ofir.oved13@gmail.com>
2025-04-08 11:43:13 +0300

    started routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import SignIn from "./SignIn"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
+  //Routes - listen to changes and the url and finds the correct route
 
   return (
     <>
+      <Routes>
+        <Route/>
+      </Routes>
       <SignIn/>
     </>
   )
---
+++ client/src/mainPage.jsx
@@ -0,0 +1,14 @@
+const MainPage = () => {
+    
+  return (
+    <>
+    <div className='new-todo-card'>
+        <div className="headline">
+            <h2>New Todo</h2>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default MainPage
